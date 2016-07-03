71c466253c11f86cc2f2b937a9b9fa96603c9316
Martin Garton <martin.garton@ft.com>
2016-07-03 09:20:20 +0100

    Fix domain lookup in some cases

--- zone.go
+++ zone.go
@@ -95,13 +95,13 @@ func NewMDNSService(instance, service, domain, hostName string, port int, ips []
 
 	if len(ips) == 0 {
 		var err error
-		ips, err = net.LookupIP(hostName)
+		ips, err = net.LookupIP(trimDot(hostName))
 		if err != nil {
 			// Try appending the host domain suffix and lookup again
 			// (required for Linux-based hosts)
 			tmpHostName := fmt.Sprintf("%s%s", hostName, domain)
 
-			ips, err = net.LookupIP(tmpHostName)
+			ips, err = net.LookupIP(trimDot(tmpHostName))
 
 			if err != nil {
 				return nil, fmt.Errorf("could not determine host IP addresses for %s", hostName)
